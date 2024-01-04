8ab9af4c06a5c22f2583ee1ae1af59fb10bb8991
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-01-04 17:31:14 -0500

    Add Pods.xcodeproj to Runner.xcworkspace

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:swapme_code/main';
+import "package:swapme/main.dart" show MyApp;
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
