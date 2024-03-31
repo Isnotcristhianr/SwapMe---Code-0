5a151ecf4d0ab95518d56b7cb31976a399c0f06a
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-03-30 19:42:07 -0500

    Agregadas animaciones y actualizada la versión de FirebaseFirestore en iOS y Package.resolved

--- lib/app/modules/notifications/views/notifications_view.dart
+++ lib/app/modules/notifications/views/notifications_view.dart
@@ -10,6 +10,9 @@ import '../controllers/notifications_controller.dart';
 // ignore: unused_import
 import 'widgets/notification_item.dart';
 
+//animaciones
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+
 //ranking mddel
 import 'package:swapme/app/data/models/ranking_model.dart';
 
@@ -48,13 +51,12 @@ class NotificationsView extends GetView<NotificationsController> {
                         future: controller.getUserById(
                             controller.topUsers[i].authId.toString()),
                         builder: (context, snapshot) {
-                         
-                            return buildUserRankingItem(
-                              i + 1,
-                              controller.topUsers[i],
-                              snapshot.data
-                                  .toString(), // Pasa el nombre del usuario como parámetro
-                            );
+                          return buildUserRankingItem(
+                            i + 1,
+                            controller.topUsers[i],
+                            snapshot.data
+                                .toString(), // Pasa el nombre del usuario como parámetro
+                          );
                         },
                       ),
                     // Aquí puedes mostrar la lista completa de usuarios si es necesario
@@ -75,12 +77,58 @@ class NotificationsView extends GetView<NotificationsController> {
     RankingModel rank,
     String userName,
   ) {
-    return ListTile(
-      leading: Text('$position'), // Muestra la posición en el ranking
-      title: Text("Usuario: $userName "), // Mostrar el nombre del usuario
+    Color color;
+    if (position == 1) {
+      //color primer lugar oro
+      color = Colors.amber;
+    } else if (position == 2) {
+      color = Colors.grey; // Color para el segundo usuario
+    } else if (position == 3) {
+      color = Colors.brown; // Color para el tercer usuario
+    } else {
+      color = Colors.white; // Color para los demás usuarios
+    }
 
-      subtitle: Text(
-          'Intercambios: ${rank.totalSwaps}, Puntuación: ${rank.punt}'), // Muestra más detalles del usuario
+    return AnimationConfiguration.staggeredList(
+      position: position,
+      duration: const Duration(milliseconds: 500),
+      child: SlideAnimation(
+        verticalOffset: 50.0,
+        child: FadeInAnimation(
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 8.0),
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: color,
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 2,
+                  blurRadius: 4,
+                  offset: const Offset(0, 2),
+                ),
+              ],
+            ),
+            child: ListTile(
+              leading: 
+              //trofeo para cada lugar
+              position == 1
+                  ? const Icon(Icons.emoji_events, size: 40)
+                  : position == 2
+                      ? const Icon(Icons.emoji_events, size: 40)
+                      : position == 3
+                          ? const Icon(Icons.emoji_events, size: 40)
+                          : const Icon(Icons.emoji_events, size: 40),
+                          
+              title:
+                  Text("Usuario: $userName "), // Mostrar el nombre del usuario
+              subtitle: Text(
+                  'Intercambios: ${rank.totalSwaps}, Puntuación: ${rank.punt}'), // Muestra más detalles del usuario
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
