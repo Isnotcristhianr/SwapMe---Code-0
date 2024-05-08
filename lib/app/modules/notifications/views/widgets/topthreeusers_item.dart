c7fe15060b7276d1cbbf9e72a01b58c97bb72c40
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2024-05-08 00:05:13 -0500

    chore: Update TopThreeUsersItem widget with improved user ranking and bar chart

--- lib/app/modules/notifications/views/widgets/topthreeusers_item.dart
+++ lib/app/modules/notifications/views/widgets/topthreeusers_item.dart
@@ -19,7 +19,7 @@ class TopThreeUsersItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: 100,
-      height: 200, // Altura máxima para la barra y la imagen
+      height: 300, // Altura máxima para la barra y la imagen
       child: Stack(
         alignment: Alignment.center,
         children: [
@@ -41,28 +41,40 @@ class TopThreeUsersItem extends StatelessWidget {
           ),
           Positioned(
             top: 100,
-            child: Container(
-              height: _getBarHeight(position),
-              width: 60, // Ancho de la barra
-              decoration: BoxDecoration(
-                color: _getBarColor(position),
-                borderRadius: BorderRadius.circular(15), // Forma redondeada para la barra
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(0, 3), // Desplazamiento de la sombra
+            child: Column(
+              children: [
+                Container(
+                  height: _getBarHeight(position),
+                  width: 60, // Ancho de la barra
+                  decoration: BoxDecoration(
+                    color: _getBarColor(position),
+                    borderRadius: BorderRadius.circular(15), // Forma redondeada para la barra
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: const Offset(0, 3), // Desplazamiento de la sombra
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: const Center(
-                child: Icon(
-                  Icons.emoji_events, // Icono del trofeo
-                  color: Colors.white,
-                  size: 20,
+                  child: const Center(
+                    child: Icon(
+                      Icons.emoji_events, // Icono del trofeo
+                      color: Colors.white,
+                      size: 20,
+                    ),
+                  ),
                 ),
-              ),
+                const SizedBox(height: 5),
+                Text(
+                  position.toString(),
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
@@ -73,11 +85,11 @@ class TopThreeUsersItem extends StatelessWidget {
   double _getBarHeight(int position) {
     switch (position) {
       case 1:
-        return 100; // Altura de la barra para el primer lugar
+        return 175; // Altura de la barra para el primer lugar
       case 2:
-        return 85; // Altura de la barra para el segundo lugar
+        return 135; // Altura de la barra para el segundo lugar
       case 3:
-        return 65; // Altura de la barra para el tercer lugar
+        return 100; // Altura de la barra para el tercer lugar
       default:
         return 0;
     }
@@ -86,11 +98,11 @@ class TopThreeUsersItem extends StatelessWidget {
   Color _getBarColor(int position) {
     switch (position) {
       case 1:
-        return Colors.red; // Color de la barra para el primer lugar
+        return Colors.greenAccent; // Color de la barra para el primer lugar
       case 2:
         return Colors.blue; // Color de la barra para el segundo lugar
       case 3:
-        return Colors.green; // Color de la barra para el tercer lugar
+        return Colors.grey; // Color de la barra para el tercer lugar
       default:
         return Colors.transparent;
     }
